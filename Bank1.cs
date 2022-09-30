8ea338352e3a1b0439f6048325eb33eb640240f9
MDKAZEEMUDDIN <113545643+MDKAZEEMUDDIN@users.noreply.github.com>
2022-09-30 22:16:07 +0530

    Add files via upload

---
+++ Bank1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class Bank1
+    {
+        private int customerid;
+        private string customername;
+        private int numberofcustomer;
+        private int noofemployee;
+        private int noofmanager;
+        private int nonmanager;
+        private int noofclerck;
+        public int result;
+        
+        public int customerId
+        {
+            set { customerid = value; }
+            get { return customerid; }
+            
+            
+        }
+        public string customerName
+        {
+            set { customername = value; }
+            get { return customername; }
+        }
+        public int Numberofcustomer
+        {
+            set { numberofcustomer = value; }
+            get { return numberofcustomer; }    
+        }
+        public int Numberofemployee
+        {
+            set { noofemployee = value; }
+            get { return noofemployee; }
+        }
+        public int Noofmanager
+        {
+            set { noofmanager = value; }
+            get {  return noofmanager;}
+        }
+        public int Nonmanager
+        {
+            set { nonmanager = value; }
+            get { return nonmanager;}   
+        }
+        public int Noofclerck
+        {
+            set { noofclerck = value; }
+            get { return noofclerck;}   
+        }
+        public int Add()
+        {
+           return result = customerId + Numberofcustomer + Numberofemployee + Noofmanager + Nonmanager + Noofclerck;
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank1 b = new Bank1();
+             b.customerId = 123;
+            b.customerName = "sameer";
+            b.Numberofcustomer = 53;
+            b.Numberofemployee = 10;
+            b.Noofmanager = 1;
+            b.Nonmanager = 0;
+            b.Noofclerck = 5;
+            Console.WriteLine(" "+b.customerId);
+            Console.WriteLine(""+b.customerName);
+            Console.WriteLine(""+b.Numberofcustomer);
+            Console.WriteLine(""+b.Numberofemployee);
+            Console.WriteLine(""+b.Noofmanager);
+            Console.WriteLine(""+b.Nonmanager);
+            Console.WriteLine(""+b.Noofclerck);
+            Console.WriteLine(b.result);
+            b.Add();
+    }
+}
+}
