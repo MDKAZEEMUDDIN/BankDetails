d695c1572eb39665d49f57d25974ffa7e8eda1a6
MDKAZEEMUDDIN <113545643+MDKAZEEMUDDIN@users.noreply.github.com>
2022-10-03 23:06:23 +0530

    Add files via upload

---
+++ Interface1/Interface1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Interface1.Dog;
+
+namespace Interface1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog d=new Dog();
+            d.animalvoice();
+            Snake  s = new Snake();
+            s.animalvoice();
+            Console.ReadLine();
+        }
+    }
+}
