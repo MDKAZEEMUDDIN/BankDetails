7903d656aa9407a064ce96def4363eb9b82c66e9
MDKAZEEMUDDIN <113545643+MDKAZEEMUDDIN@users.noreply.github.com>
2022-10-03 22:58:04 +0530

    Add files via upload

---
+++ Dog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface1
+{
+    internal class Dog : IAnimal
+    {
+        public void animalvoice()
+        {
+            Console.WriteLine("Dog make sound bow bow ");
+        }
+        internal class Snake : IAnimal
+        {
+            public void animalvoice()
+            {
+                Console.WriteLine("Snake make noice Hiss");
+            }
+        }
+
+    }
+}
